80848e52c57be4a5cc22c7bc6288cace42e806d1
Marnitz Malan <marnitz.malan@payfast.co.za>
2025-06-08 19:26:41 +0200

    build configs

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,36 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
-            },
-        }),
-        tailwindcss(),
-    ],
+            }),
+            tailwindcss(),
+        ],
+        server: {
+            host: env.VITE_SERVER_HOST || 'localhost',
+            port: parseInt(env.VITE_SERVER_PORT || '5173'),
+            strictPort: true,
+        },
+        preview: {
+            host: 'localhost',
+            port: 5173,
+            strictPort: true,
+        },
+    };
 });
